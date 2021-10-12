4176908fd35c2d7af1b7141248b7af18b8e6346b
Mario Palacios <48101008+MarioPal97@users.noreply.github.com>
2021-10-11 20:53:18 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+#import sys
+#sys.path.append('/usr/local/lib/python2.7/site-packages')
+#sys.path.append('/usr/local/lib/python3.5/dist-packages/pytesseract/')
+#sys.path.append('/usr/src/tesseract/tessdata ')
+#sys.path.append('/usr/bin/tesseract')
+import cv2
+#sys.path.append('/usr/src/tesseract')
+import pytesseract
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 30
+
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	image = frame.array
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	
+	rawCapture.truncate(0)
+
+	if key == ord("s"):
+		text = pytesseract.image_to_string(image)
+		print(text)
+		cv2.imshow("Frame", image)
+		cv2.waitKey(0)
+		break
+
+cv2.destroyAllWindows()
