8e76ca40f059a8c0c58538a98e1a23cf42f71ae6
petersonandrew <80303710+petersonandrew@users.noreply.github.com>
2021-10-25 18:39:50 -0700

    takes picture and returns the text

---
+++ image_to_text.py
@@ -0,0 +1,34 @@
+import picamera
+import ocrspace
+import requests
+import json
+import cv2
+import io
+import numpy as np
+
+def Read():
+    with picamera.PiCamera() as camera:
+        camera.capture('image.jpg')
+
+    img = cv2.imread("image.jpg")
+    height, width, _ = img.shape
+    roi = img
+
+    url_api = "https://api.ocr.space/parse/image"
+    _, compressedimage = cv2.imencode(".jpg", roi, [1, 80])
+    file_bytes = io.BytesIO(compressedimage)
+
+
+    result = requests.post(url_api,
+                files = {"one.jpg": file_bytes},
+                data = {"apikey": "4a09d6b7c988957",
+                        "language": "eng"})
+    result = result.content.decode()
+    result = json.loads(result)
+    error = result.get("ErrorMessage")
+    #print(error)
+    parsed_results = result.get("ParsedResults")[0]
+    text_detected = parsed_results.get("ParsedText")
+    #print(text_detected)
+
+    return text_detected
