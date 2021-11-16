0992bc80368432338a755c8b797df96ce76843a4
Mario Palacios <48101008+MarioPal97@users.noreply.github.com>
2021-11-15 20:01:33 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,7 +3,7 @@
 #navigate to home directory, then to this directory
 #then execute script
 #to turn launcher.sh into an executable type in:
-#sudo chmod 755
+#sudo chmod 755 launcher.sh
 
 cd /
 cd /home/pi/Documents/Senior-Design   #where the script is
