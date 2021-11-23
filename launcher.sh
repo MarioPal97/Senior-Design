abcd1765b985ac254a6d74a38d601d74723634bd
Mario Palacios <48101008+MarioPal97@users.noreply.github.com>
2021-11-22 17:52:58 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -9,3 +9,12 @@ cd /
 cd /home/pi/Documents/Senior-Design   #where the script is
 sudo python3 tts_text.py              #command to run the script
 
+# make a logs directory:
+# mkdir logs
+# then create it as a root directory:
+# sudo -I
+# mkdir logs
+# Scheduling using crontab > might have to put vim before crontab
+# sudo crontab -e
+# go all the way to the bottom to schedule task: 
+# @reboot sh “path of .sh file” > “path of log directory” 2>&1
