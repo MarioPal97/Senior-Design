a251eef2083a987a2690e6affdcc639ee83758df
petersonandrew <80303710+petersonandrew@users.noreply.github.com>
2021-12-08 12:08:10 -0800

    Update image_to_text.py

--- image_to_text.py
+++ image_to_text.py
@@ -12,7 +12,7 @@ def Read():
         camera.capture('image.jpg')
 
     img = cv2.imread("image.jpg")
-    img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE) 
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE) 
     height, width, _ = img.shape
     roi = img
 
