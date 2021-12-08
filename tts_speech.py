5a7e1509c19bb8b330767cbd0aaf46165d86cdc5
petersonandrew <80303710+petersonandrew@users.noreply.github.com>
2021-12-07 23:40:36 -0800

    Update tts_speech.py

--- tts_speech.py
+++ tts_speech.py
@@ -1,6 +1,7 @@
-import os
+#import os
+from subprocess import call
 
 def Say(incomingTxt, speed):
   #os.popen( 'espeak "'+incomingTxt+'" -s "'+speed+'" --stdout | aplay 2>/dev/null')
-  os.popen('espeak', incomingTxt, '-s', speed, '--stdout | aplay 2>/dev/null')
-
+  #os.popen('espeak', incomingTxt, '-s', speed, '--stdout | aplay 2>/dev/null')
+  call(["espeak", "-s", speed, incomingTxt, "--stdout | aplay 2>/dev/null"]) 
