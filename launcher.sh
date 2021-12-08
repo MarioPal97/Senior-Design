d4812a5d7f8d671b103dc20e8d93c3e16fcbff84
Mario Palacios <48101008+MarioPal97@users.noreply.github.com>
2021-12-07 22:54:36 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,9 +5,9 @@
 #to turn launcher.sh into an executable type in:
 #sudo chmod 755 launcher.sh
 
-cd /
-cd /home/pi/Documents/Senior-Design   #where the script is
-sudo python3 tts_text.py              #command to run the script
+#cd /
+#cd /home/pi/Documents/Senior-Design   #where the script is
+#sudo python3 tts_text.py              #command to run the script
 
 # make a logs directory:
 # mkdir logs
